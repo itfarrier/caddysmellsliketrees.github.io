b05916af6cd62e684fbe64ba177decd3c2a62af6
Dmitrij Podabed <dmitrij@podabed.org>
2017-12-17 02:41:16 +0700

    full-logo and burger-button sizes corrections

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -21,10 +21,10 @@ class Sidebar extends React.Component {
         burgerBarClassName={styles.burger}
         burgerButtonClassName={isMobile.any ? styles.button : styles.button2}
         customBurgerIcon={
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792">
+          <svg viewBox="0 0 1792 1792">
             <path
-              d="M1664 1344v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V832q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V320q0-26 19-45t45-19h1408q26 0 45 19t19 45z"
               fill="#0a0a0a"
+              d="M1664 1344v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45v-128q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V832q0-26 19-45t45-19h1408q26 0 45 19t19 45zm0-512v128q0 26-19 45t-45 19H192q-26 0-45-19t-19-45V320q0-26 19-45t45-19h1408q26 0 45 19t19 45z"
             />
           </svg>
         }
