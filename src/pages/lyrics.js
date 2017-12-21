e1c5474403cc5d924c1c4b2f3d75a72832db92eb
Dmitrij Podabed <dmitrij@podabed.org>
2017-12-21 20:32:47 +0700

    top padding for lyrics list

--- src/pages/lyrics.js
+++ src/pages/lyrics.js
@@ -1,3 +1,4 @@
+import isMobile from "ismobilejs";
 import Link from "gatsby-link";
 import PropTypes from "prop-types";
 import React from "react";
@@ -7,8 +8,8 @@ import styles from "./lyrics.module.css";
 const lyrics = ({ data }) => (
   <div className="container">
     <div className="row">
-      <div className={"col " + styles.toc}>
-        <ul className={styles.list}>
+      <div className={isMobile.any ? "col" : "col " + styles.toc}>
+        <ul className={isMobile.any ? styles.list : styles.listDesktop}>
           {data.allMarkdownRemark.edges.map(({ node }) => {
             return (
               <li className={styles.link} key={node.id}>
