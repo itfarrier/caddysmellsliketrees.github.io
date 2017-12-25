69ad2a5e3b2097b9dc52cef1e1261aa7442538c5
Dmitrij Podabed <dmitrij@podabed.org>
2017-12-26 01:50:38 +0700

    preact instead of react

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     title: "Кэдди пахнет деревьями"
   },
   plugins: [
+    "gatsby-plugin-preact",
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-sass",
