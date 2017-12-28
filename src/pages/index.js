53b94bd9f3efc3c257862188113f7ba42fe7e8e3
Dmitrij Podabed <dmitrij@podabed.org>
2017-12-28 19:02:48 +0700

    text logo in svg instead of styled text

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,26 @@ const Index = () => (
   <div className="logo-centering">
     <div className="full-logo">
       <div className="img-logo img-logo_margin-right">
-        <img alt="tree" className="img-logo__tree" src="/vectors/tree.svg" />
+        <img alt="Дерево" className="img-logo__tree" src="/vectors/tree.svg" />
         <div
-          title="ЛУЧ"
+          title="Луч"
           className="img-logo__line img-logo__line_dot-reverse"
         />
       </div>
       <div className="text-logo">
-        <div className="text-logo__caddy">КЭДДИ</div>
-        <div className="text-logo__smells">ПАХНЕТ</div>
-        <div className="text-logo__like_trees">ДЕРЕВЬЯМИ</div>
+        <img
+          alt="Кэдди пахнет деревьями"
+          className="img-logo__tree"
+          src="/vectors/textLogo.svg"
+        />
       </div>
       <div className="img-logo img-logo_margin-left">
         <img
-          alt="tree"
+          alt="Дерево"
           className="img-logo__tree img-logo__tree_hidden"
           src="/vectors/tree.svg"
         />
-        <div title="ЛУЧ" className="img-logo__line" />
+        <div title="Луч" className="img-logo__line" />
       </div>
     </div>
   </div>
