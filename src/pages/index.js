ad78c1c0ce7dbb2c6f0e4e7f1b484971a09697c6
Dmitrij Podabed <dmitrij@podabed.org>
2017-12-28 19:15:59 +0700

    alt texts corrected

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ const Index = () => (
   <div className="logo-centering">
     <div className="full-logo">
       <div className="img-logo img-logo_margin-right">
-        <img alt="tree" className="img-logo__tree" src="/vectors/tree.svg" />
+        <img alt="Дерево" className="img-logo__tree" src="/vectors/tree.svg" />
         <div
-          title="ЛУЧ"
+          title="Луч"
           className="img-logo__line img-logo__line_dot-reverse"
         />
       </div>
@@ -17,11 +17,11 @@ const Index = () => (
       </div>
       <div className="img-logo img-logo_margin-left">
         <img
-          alt="tree"
+          alt="Скрытое дерево"
           className="img-logo__tree img-logo__tree_hidden"
           src="/vectors/tree.svg"
         />
-        <div title="ЛУЧ" className="img-logo__line" />
+        <div title="Луч" className="img-logo__line" />
       </div>
     </div>
   </div>
