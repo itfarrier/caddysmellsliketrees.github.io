a9069eb3fee20e2075544b9062dfecbaf1d01b75
Dmitrij Podabed <dmitrij@podabed.org>
2018-03-10 21:16:17 +0700

    Updates

--- path----f2e4818c86ad71c36357.js
+++ path----f2e4818c86ad71c36357.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{78:function(t,e){t.exports={data:{site:{siteMetadata:{description:"Психоделический фри-фолк",siteUrl:"https://caddysmellsliketrees.ru",title:"Кэдди пахнет деревьями"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{76:function(t,e){t.exports={data:{site:{siteMetadata:{description:"Психоделический фри-фолк",siteUrl:"https://caddysmellsliketrees.ru",title:"Кэдди пахнет деревьями"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----f2e4818c86ad71c36357.js.map
