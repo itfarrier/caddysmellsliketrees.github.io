dd784b53744c6567366bf35d144434fa74905a4f
Dmitrij Podabed <dmitrij@podabed.org>
2018-08-12 17:57:34 +0300

    add countdown to index page

--- src/pages/index.en.tsx
+++ src/pages/index.en.tsx
@@ -1,15 +1,32 @@
 import * as React from "react";
+import Countdown from "react-countdown-now";
 
 import * as styles from "./index.module.scss";
 
 const IndexEn: React.SFC = () => (
   <article className={styles.article}>
-    <p>We play soul-helpful every day songs.</p>
-    <p>
-      They grow in our forests so that they are difficult to separate from each
-      other.
-    </p>
-    <p>And between them glint bold deer poems.</p>
+    <Countdown
+      date="01 Sep 2018 20:00"
+      renderer={({ days, hours, minutes }) => (
+        <div className={styles.countdown}>
+          <div className={styles.block}>
+            <span className={styles.data}>{days}</span>
+            <span className={styles.title}>{days > 1 ? "days" : "day"}</span>
+          </div>
+          <div className={styles.block}>
+            <span className={styles.data}>{hours}</span>
+            <span className={styles.title}>{hours > 1 ? "hours" : "hour"}</span>
+          </div>
+          <div className={styles.block}>
+            <span className={styles.data}>{minutes}</span>
+            <span className={styles.title}>
+              {minutes > 1 ? "minutes" : "minute"}
+            </span>
+          </div>
+        </div>
+      )}
+      zeroPadLength={0}
+    />
   </article>
 );
 
--- src/pages/index.ru.tsx
+++ src/pages/index.ru.tsx
@@ -1,14 +1,48 @@
 import * as React from "react";
+import Countdown from "react-countdown-now";
 
 import * as styles from "./index.module.scss";
 
 const IndexRu: React.SFC = () => (
   <article className={styles.article}>
-    <p>Мы играем душеполезные песни на каждый день.</p>
-    <p>
-      Они у нас срастаются в леса так, что их тяжело отделить друг от друга.
-    </p>
-    <p>А между ними бликуют смелыми оленями стихи.</p>
+    <Countdown
+      date="01 Sep 2018 20:00"
+      renderer={({ days, hours, minutes }) => {
+        function declOfNum(time: number, titles: string[]) {
+          const cases = [2, 0, 1, 1, 1, 2];
+
+          return titles[
+            time % 100 > 4 && time % 100 < 20
+              ? 2
+              : cases[time % 10 < 5 ? time % 10 : 5]
+          ];
+        }
+
+        return (
+          <div className={styles.countdown}>
+            <div className={styles.block}>
+              <span className={styles.data}>{days}</span>
+              <span className={styles.title}>
+                {declOfNum(days, ["день", "дня", "дней"])}
+              </span>
+            </div>
+            <div className={styles.block}>
+              <span className={styles.data}>{hours}</span>
+              <span className={styles.title}>
+                {declOfNum(hours, ["час", "часа", "часов"])}
+              </span>
+            </div>
+            <div className={styles.block}>
+              <span className={styles.data}>{minutes}</span>
+              <span className={styles.title}>
+                {declOfNum(minutes, ["минута", "минуты", "минут"])}
+              </span>
+            </div>
+          </div>
+        );
+      }}
+      zeroPadLength={0}
+    />
   </article>
 );
 
