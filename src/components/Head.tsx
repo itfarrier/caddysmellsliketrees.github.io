6e63934ffe80269109ab583d35f632063291cc35
Dmitrij Podabed <d.podabed@belitsoft.com>
2018-10-02 17:47:26 +0300

    add functions for generating faviocons tags, fix keywords and description metas

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -28,13 +28,7 @@ const Head: React.SFC<IHead> = ({
   page,
   subPage
 }) => {
-  const variantOfPageTitle: string = subPage
-    ? `${title} — ${description} — ${page} — ${subPage}`
-    : page
-      ? `${title} — ${description} — ${page}`
-      : `${title} — ${description}`;
-
-  function generateAndroidChrome(sizesInPx: number[]) {
+  const generateAndroidChrome = (sizesInPx: number[]) => {
     return sizesInPx.map(size => (
       <link
         href={`/favicons/android-chrome-${size}x${size}.png`}
@@ -43,34 +37,43 @@ const Head: React.SFC<IHead> = ({
         type="image/png"
       />
     ));
-  }
-  function generateAppleTouch(sizesInPx: number[]) {
+  };
+  const generateAppleTouch = (sizesInPx: number[]) => {
     return sizesInPx.map(size => [
       <link
         href={`/favicons/apple-touch-icon-${size}x${size}.png`}
-        key={`${size.index}${size}`}
+        key={`1${size}`}
         rel="apple-touch-icon"
         sizes={`${size}x${size}`}
         type="image/png"
       />,
       <link
         href={`/favicons/apple-touch-icon-${size}x${size}-precomposed.png`}
-        key={`${size.index}${size}`}
+        key={`2${size}`}
         rel="apple-touch-icon"
         sizes={`${size}x${size}`}
         type="image/png"
       />
     ]);
-  }
+  };
+  const generateFavicon = (sizesInPx: number[]) => {
+    return sizesInPx.map(size => (
+      <link
+        href={`/favicons/favicon-${size}x${size}.png`}
+        rel="icon"
+        sizes={`${size}x${size}`}
+        type="image/png"
+      />
+    ));
+  };
+  const variantOfPageTitle: string = subPage
+    ? `${title} — ${description} — ${page} — ${subPage}`
+    : page
+      ? `${title} — ${description} — ${page}`
+      : `${title} — ${description}`;
 
   return (
-    <Helmet
-      meta={[
-        { name: "description", content: description },
-        { name: "keywords", content: keywords }
-      ]}
-      title={variantOfPageTitle}
-    >
+    <Helmet title={variantOfPageTitle}>
       {generateAndroidChrome([36, 48, 72, 96, 144, 192, 256, 384, 512])}
       <link
         href="/favicons/apple-touch-icon.png"
@@ -85,30 +88,15 @@ const Head: React.SFC<IHead> = ({
         type="image/png"
       />
       {generateAppleTouch([57, 60, 72, 76, 114, 120, 144, 152, 180])}
-      <link
-        href="/favicons/favicon-32x32.png"
-        rel="icon"
-        sizes="32x32"
-        type="image/png"
-      />
-      <link
-        href="/favicons/favicon-194x194.png"
-        rel="icon"
-        sizes="194x194"
-        type="image/png"
-      />
-      <link
-        href="/favicons/favicon-16x16.png"
-        rel="icon"
-        sizes="16x16"
-        type="image/png"
-      />
+      {generateFavicon([16, 32, 194])}
       <link
         color="#0a0a0a"
         href="/favicons/safari-pinned-tab.svg"
         rel="mask-icon"
       />
       <link href="/favicons/favicon.ico" rel="shortcut icon" />
+      <meta content={description} name="description" />
+      <meta content={keywords.toString()} name="keywords" />
       <meta content={title} name="apple-mobile-web-app-title" />
       <meta content={title} name="application-name" />
       <meta content="#f0f0f0" name="msapplication-TileColor" />
