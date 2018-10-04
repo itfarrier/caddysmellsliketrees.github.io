2d51044822a5e51771ea214806cc68cac0e03246
Dmitrij Podabed <d.podabed@belitsoft.com>
2018-10-04 14:24:22 +0300

    remove langs layouts, turn on offline

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,7 +1,13 @@
 import "intl";
+import "intl/locale-data/jsonp/en";
+import "intl/locale-data/jsonp/ru";
 import { getCurrentLangKey, getLangs, getUrlForLang } from "ptz-i18n";
 import * as React from "react";
-import { IntlProvider } from "react-intl";
+import { addLocaleData, IntlProvider } from "react-intl";
+import * as en from "react-intl/locale-data/en";
+import * as ru from "react-intl/locale-data/ru";
+
+addLocaleData([...en, ...ru]);
 
 import Footer from "../components/Footer";
 import Head from "../components/Head";
@@ -50,19 +56,22 @@ const IndexLayout: React.SFC<IIndexLayout> = props => {
         }
       }
     },
-    location: { pathname },
-    i18nMessages,
-    i18nMessages: { pageNames }
+    location: { pathname }
   } = props;
   const langKey = getCurrentLangKey(langs, defaultLangKey, pathname);
   const homeLink = `/${langKey}/`;
   const langsMenu = getLangs(langs, langKey, getUrlForLang(homeLink, pathname));
+  const i18nMessages = require(`../data/messages/${langKey}`);
 
   return (
     <IntlProvider locale={langKey} messages={i18nMessages}>
       <React.Fragment>
         <Head currentLanguage={langKey} i18nMessages={i18nMessages} />
-        <Header homeLink={homeLink} pageNames={pageNames} pathname={pathname} />
+        <Header
+          homeLink={homeLink}
+          pageNames={i18nMessages.pageNames}
+          pathname={pathname}
+        />
         <div className={styles.space} />
         <main className={styles.main}>
           {children({ i18nMessages, ...props })}
