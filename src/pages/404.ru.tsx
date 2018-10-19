8077d09cc5aeeca5d82de14e91d120953c8410d2
Dmitrij Podabed <d.podabed@belitsoft.com>
2018-10-19 13:02:48 +0300

    404 for different langs

---
+++ src/pages/404.ru.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+const NotFoundPage: React.SFC = () => (
+    <div>
+        <h1>НЕ НАЙДЕНО</h1>
+        <p>Вы хотите смотреть страницу, которой не существует... Печально.</p>
+    </div>
+);
+
+export default NotFoundPage;
