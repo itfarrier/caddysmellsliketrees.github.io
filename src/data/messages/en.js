ac3b1cf254be37341eee3e46a66839ebc5bd8d1a
Dmitrij Podabed <d.podabed@belitsoft.com>
2018-10-19 19:02:40 +0300

    add i18n data for photo albums

--- src/data/messages/en.js
+++ src/data/messages/en.js
@@ -15,7 +15,7 @@ module.exports = {
             date: {
                 day: 5,
                 month: 11,
-                year: 2017,
+                year: 2016,
             },
             slug: '2016-11-05-museum-night-talk-after-midnight/',
             title: 'museum night: talks after midnight',
@@ -28,6 +28,222 @@ module.exports = {
             },
             slug: '2017-03-29-rehearsal/',
             title: 'rehearsal',
+        },
+        {
+            date: {
+                day: 19,
+                month: 4,
+                year: 2017,
+            },
+            slug: '2017-04-19-rehearsal/',
+            title: 'rehearsal',
+        },
+        {
+            date: {
+                day: 20,
+                month: 5,
+                year: 2017,
+            },
+            slug: '2017-05-20-tea-yurt-birthday/',
+            title: 'tea yurt birthday',
+        },
+        {
+            date: {
+                day: 28,
+                month: 5,
+                year: 2017,
+            },
+            slug: '2017-05-28-music-in-museum/',
+            title: 'music in museum',
+        },
+        {
+            date: {
+                day: 17,
+                month: 6,
+                year: 2017,
+            },
+            slug: '2017-06-17-festival-spirit-of-time/',
+            title: 'festival “spirit of time”',
+        },
+        {
+            date: {
+                day: 12,
+                month: 8,
+                year: 2017,
+            },
+            slug: '2017-08-12-museum-svetla-songs/',
+            title: 'museum “svetla’s songs”',
+        },
+        {
+            date: {
+                day: 18,
+                month: 8,
+                year: 2017,
+            },
+            slug: '2017-08-18-art-coast/',
+            title: 'Art‐coast',
+        },
+        {
+            date: {
+                day: 3,
+                month: 9,
+                year: 2017,
+            },
+            slug: '2017-09-03-non-museum-night-here-and-now/',
+            title: 'Non museum night. Here and now',
+        },
+        {
+            date: {
+                day: 19,
+                month: 10,
+                year: 2017,
+            },
+            slug: '2017-10-19-flavored-flat/',
+            title: 'Flavored flat',
+        },
+        {
+            date: {
+                day: 2,
+                month: 12,
+                year: 2017,
+            },
+            slug: '2017-12-02-music-festival-fresh-names/',
+            title: 'Music festival “Fresh names”',
+        },
+        {
+            date: {
+                day: 24,
+                month: 12,
+                year: 2017,
+            },
+            slug: '2017-12-24-caddy-smells-like-den/',
+            title: 'Caddy smells like den',
+        },
+        {
+            date: {
+                day: 21,
+                month: 1,
+                year: 2018,
+            },
+            slug: '2018-01-21-station-of-culture-gagarin/',
+            title: 'Station of culture “Gagarin”',
+        },
+        {
+            date: {
+                day: 24,
+                month: 2,
+                year: 2018,
+            },
+            slug: '2018-02-24-musical-poetic-performance-me-in-child/',
+            title: 'Musical‐poetic performance “Me in child”',
+        },
+        {
+            date: {
+                day: 4,
+                month: 3,
+                year: 2018,
+            },
+            slug: '2018-03-04-yushin-brothers/',
+            title: 'Yushin brothers',
+        },
+        {
+            date: {
+                day: 21,
+                month: 3,
+                year: 2018,
+            },
+            slug: '2018-03-21-international-festival-poetry-world-day/',
+            title: 'International festival “Poetry world day”',
+        },
+        {
+            date: {
+                day: 20,
+                month: 4,
+                year: 2018,
+            },
+            slug: '2018-04-20-biblionight/',
+            title: 'Biblionight',
+        },
+        {
+            date: {
+                day: 13,
+                month: 5,
+                year: 2018,
+            },
+            slug: '2018-05-13-rehearsal/',
+            title: 'Rehearsal',
+        },
+        {
+            date: {
+                day: 6,
+                month: 6,
+                year: 2018,
+            },
+            slug: '2018-06-06-amphibrah-on-grass/',
+            title: 'Amphibrah on grass',
+        },
+        {
+            date: {
+                day: 16,
+                month: 6,
+                year: 2018,
+            },
+            slug: '2018-06-16-non-percussion-concert-on-the-art-coast/',
+            title: 'Non percussion concert on the Art‐coast',
+        },
+        {
+            date: {
+                day: 29,
+                month: 7,
+                year: 2018,
+            },
+            slug: '2018-07-29-o-music-first-music-festival-onthekamenka/',
+            title: '“Oh, music”. First music festival #onthekamenka',
+        },
+        {
+            date: {
+                day: 3,
+                month: 8,
+                year: 2018,
+            },
+            slug: '2018-08-03-svetla-songs/',
+            title: 'Svetla’s songs',
+        },
+        {
+            date: {
+                day: 21,
+                month: 8,
+                year: 2018,
+            },
+            slug: '2018-08-21-flowerbed-on-the-south-coast/',
+            title: 'Flowerbed on the South coast',
+        },
+        {
+            date: {
+                day: 26,
+                month: 8,
+                year: 2018,
+            },
+            slug: '2018-08-26-art-festival-baikal-live/',
+            title: 'Art‐festival “Baikal-live”',
+        },
+        {
+            date: {
+                day: 1,
+                month: 4,
+                year: 2018,
+            },
+            slug: '2018-09-01-summer-kitchen/',
+            title: 'Summer kitchen',
+        },
+        {
+            date: {
+                day: 8,
+                month: 9,
+                year: 2018,
+            },
+            slug: '2018-09-08-art-coast-closing/',
+            title: 'Art‐coast closing',
         },
     ],
     title: 'Caddy smells like trees',
--- src/data/messages/ru.js
+++ src/data/messages/ru.js
@@ -15,10 +15,10 @@ module.exports = {
             date: {
                 day: 5,
                 month: 11,
-                year: 2017,
+                year: 2016,
             },
             slug: '2016-11-05-museum-night-talk-after-midnight/',
-            title: 'музейная ночь: полночные разговоры',
+            title: 'Музейная ночь: полночные разговоры',
         },
         {
             date: {
@@ -27,7 +27,223 @@ module.exports = {
                 year: 2017,
             },
             slug: '2017-03-29-rehearsal/',
-            title: 'репетиция',
+            title: 'Репетиция',
+        },
+        {
+            date: {
+                day: 19,
+                month: 4,
+                year: 2017,
+            },
+            slug: '2017-04-19-rehearsal/',
+            title: 'Репетиция',
+        },
+        {
+            date: {
+                day: 20,
+                month: 5,
+                year: 2017,
+            },
+            slug: '2017-05-20-tea-yurt-birthday/',
+            title: 'День рождения «Чайной юрты»',
+        },
+        {
+            date: {
+                day: 28,
+                month: 5,
+                year: 2017,
+            },
+            slug: '2017-05-28-music-in-museum/',
+            title: 'Музыка в музее',
+        },
+        {
+            date: {
+                day: 17,
+                month: 6,
+                year: 2017,
+            },
+            slug: '2017-06-17-festival-spirit-of-time/',
+            title: 'Фестиваль «Дух времени»',
+        },
+        {
+            date: {
+                day: 12,
+                month: 8,
+                year: 2017,
+            },
+            slug: '2017-08-12-museum-svetla-songs/',
+            title: 'Музейник «Светлы песни»',
+        },
+        {
+            date: {
+                day: 18,
+                month: 8,
+                year: 2017,
+            },
+            slug: '2017-08-18-art-coast/',
+            title: 'Арт‐берег',
+        },
+        {
+            date: {
+                day: 3,
+                month: 9,
+                year: 2017,
+            },
+            slug: '2017-09-03-non-museum-night-here-and-now/',
+            title: 'Немузейная ночь. Здесь и сейчас',
+        },
+        {
+            date: {
+                day: 19,
+                month: 10,
+                year: 2017,
+            },
+            slug: '2017-10-19-flavored-flat/',
+            title: 'Ароматный квартирник',
+        },
+        {
+            date: {
+                day: 2,
+                month: 12,
+                year: 2017,
+            },
+            slug: '2017-12-02-music-festival-fresh-names/',
+            title: 'Музыкальный фестиваль «Новые имена»',
+        },
+        {
+            date: {
+                day: 24,
+                month: 12,
+                year: 2017,
+            },
+            slug: '2017-12-24-caddy-smells-like-den/',
+            title: 'Кэдди пахнет «Берлогой»',
+        },
+        {
+            date: {
+                day: 21,
+                month: 1,
+                year: 2018,
+            },
+            slug: '2018-01-21-station-of-culture-gagarin/',
+            title: 'Культурная станция «Гагарин»',
+        },
+        {
+            date: {
+                day: 24,
+                month: 2,
+                year: 2018,
+            },
+            slug: '2018-02-24-musical-poetic-performance-me-in-child/',
+            title: 'Музыкально‐поэтический перформанс «В детстве я»',
+        },
+        {
+            date: {
+                day: 4,
+                month: 3,
+                year: 2018,
+            },
+            slug: '2018-03-04-yushin-brothers/',
+            title: 'Yushin brothers',
+        },
+        {
+            date: {
+                day: 21,
+                month: 3,
+                year: 2018,
+            },
+            slug: '2018-03-21-international-festival-poetry-world-day/',
+            title: 'Международный фестиваль «Всемирный день поэзии»',
+        },
+        {
+            date: {
+                day: 20,
+                month: 4,
+                year: 2018,
+            },
+            slug: '2018-04-20-biblionight/',
+            title: 'Библионочь',
+        },
+        {
+            date: {
+                day: 13,
+                month: 5,
+                year: 2018,
+            },
+            slug: '2018-05-13-rehearsal/',
+            title: 'Репетиция',
+        },
+        {
+            date: {
+                day: 6,
+                month: 6,
+                year: 2018,
+            },
+            slug: '2018-06-06-amphibrah-on-grass/',
+            title: 'Амфибрахий на траве',
+        },
+        {
+            date: {
+                day: 16,
+                month: 6,
+                year: 2018,
+            },
+            slug: '2018-06-16-non-percussion-concert-on-the-art-coast/',
+            title: 'Безударный конферт на Арт‐берегу',
+        },
+        {
+            date: {
+                day: 29,
+                month: 7,
+                year: 2018,
+            },
+            slug: '2018-07-29-o-music-first-music-festival-onthekamenka/',
+            title: '«О, музыка!» Первый музыкальный фестиваль #накаменке',
+        },
+        {
+            date: {
+                day: 3,
+                month: 8,
+                year: 2018,
+            },
+            slug: '2018-08-03-svetla-songs/',
+            title: 'Светлы песни',
+        },
+        {
+            date: {
+                day: 21,
+                month: 8,
+                year: 2018,
+            },
+            slug: '2018-08-21-flowerbed-on-the-south-coast/',
+            title: 'Клумба на Южном берегу',
+        },
+        {
+            date: {
+                day: 26,
+                month: 8,
+                year: 2018,
+            },
+            slug: '2018-08-26-art-festival-baikal-live/',
+            title: 'Арт‐фестиваль «Baikal-live»',
+        },
+        {
+            date: {
+                day: 1,
+                month: 4,
+                year: 2018,
+            },
+            slug: '2018-09-01-summer-kitchen/',
+            title: 'Летняя кухня',
+        },
+        {
+            date: {
+                day: 8,
+                month: 9,
+                year: 2018,
+            },
+            slug: '2018-09-08-art-coast-closing/',
+            title: 'Закрытие Арт‐берега',
         },
     ],
     title: 'Кэдди пахнет деревьями',
